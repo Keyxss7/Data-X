86247b82871a8cd5a1b93d5c87f9983674c0af90
Keyxss7 <159769013+Keyxss7@users.noreply.github.com>
2024-04-14 16:01:27 +0200

    Add files via upload

--- traitement.php
+++ traitement.php
@@ -1,29 +1,37 @@
 <?php
 // Vérifie si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupère les données du formulaire
-    $pseudo = $_POST["firstName"];
-    $prenom = $_POST["lastName"];
-    $email = $_POST["email"];
-    $telephone = $_POST["mobile"];
-    $message = $_POST["message"];
+    // Validation des données
+    $pseudo = isset($_POST["firstName"]) ? htmlspecialchars($_POST["firstName"]) : "";
+    $prenom = isset($_POST["lastName"]) ? htmlspecialchars($_POST["lastName"]) : "";
+    $email = isset($_POST["email"]) ? htmlspecialchars($_POST["email"]) : "";
+    $telephone = isset($_POST["mobile"]) ? htmlspecialchars($_POST["mobile"]) : "";
+    $message = isset($_POST["message"]) ? htmlspecialchars($_POST["message"]) : "";
 
-    // Destinataire de l'email
+    // Vérification de l'e-mail
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "L'adresse e-mail n'est pas valide.";
+        exit;
+    }
+
+    // Destinataire de l'e-mail
     $to = "Keyxss2409@gmail.com";
 
-    // Sujet de l'email
+    // Sujet de l'e-mail
     $subject = "Nouveau message de $pseudo";
 
-    // Corps de l'email
+    // Corps de l'e-mail
     $body = "Prénom: $prenom\n";
     $body .= "Email: $email\n";
     $body .= "Téléphone: $telephone\n";
     $body .= "Message:\n$message";
 
-    // En-têtes de l'email
-    $headers = "From: $email";
+    // En-têtes de l'e-mail
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Envoyer l'email
+    // Envoyer l'e-mail
     if (mail($to, $subject, $body, $headers)) {
         echo "Votre message a été envoyé avec succès.";
     } else {
